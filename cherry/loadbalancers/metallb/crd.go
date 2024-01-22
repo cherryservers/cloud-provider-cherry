6300f96545a78181b32035dfeca0f22983799980
Avi Deitcher <avi@deitcher.net>
2024-01-22 18:24:51 +0200

    support for metallb 0.13.0+ CRDs
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

---
+++ cherry/loadbalancers/metallb/crd.go
@@ -0,0 +1,536 @@
+package metallb
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	metalapi "go.universe.tf/metallb/api/v1beta1"
+	"golang.org/x/exp/slices"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+const (
+	defaultBgpAdvertisement = "equinix-metal-bgp-adv"
+	cpemLabelKey            = "cloud-provider"
+	cpemLabelValue          = "equinix-metal"
+	svcLabelKeyPrefix       = "service-"
+	svcLabelValuePrefix     = "namespace-"
+)
+
+type CRDConfigurer struct {
+	namespace string // defaults to metallb-system
+
+	client client.Client
+}
+
+var _ Configurer = (*CRDConfigurer)(nil)
+
+func (m *CRDConfigurer) UpdatePeersByService(ctx context.Context, adds *[]Peer, svcNamespace, svcName string) (bool, error) {
+	olds, err := m.listBGPPeers(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	news := []metalapi.BGPPeer{}
+	toAdd := make(map[string]metalapi.BGPPeer)
+	for _, add := range *adds {
+		peer := convertToBGPPeer(add, m.namespace, svcName)
+		news = append(news, peer)
+		toAdd[peer.Name] = peer
+	}
+
+	// if there is no Peers, add all the new ones
+	if len(olds.Items) == 0 {
+		for _, n := range news {
+			err = m.client.Create(ctx, &n)
+			if err != nil {
+				return false, fmt.Errorf("unable to add BGPPeer %s: %w", n.GetName(), err)
+			}
+		}
+		return true, nil
+	}
+
+	var changed bool
+	for _, o := range olds.Items {
+		found := false
+		for _, n := range news {
+			if n.Name == o.GetName() {
+				found = true
+				// remove from toAdd list
+				delete(toAdd, n.Name)
+				// update
+				patch := client.MergeFrom(o.DeepCopy())
+				var update bool
+				// update services in node selectors
+				if peerAddService(&o, svcNamespace, svcName) {
+					update = true
+				}
+				// check specs
+				if !peerSpecEqual(o.Spec, n.Spec) {
+					o.Spec = n.Spec
+					update = true
+				}
+				if update {
+					err := m.client.Patch(ctx, &o, patch)
+					if err != nil {
+						return false, fmt.Errorf("unable to update IPAddressPool %s: %w", o.GetName(), err)
+					}
+					if !changed {
+						changed = true
+					}
+				}
+				break
+			}
+		}
+		// if a peer in the config no longer exists for a service,
+		// execute RemovePeersByService to update config
+		if !found {
+			updatedOrRemoved, err := m.updateOrDeletePeerByService(ctx, o, svcNamespace, svcName)
+			if err != nil {
+				return false, err
+			}
+			if !changed {
+				changed = updatedOrRemoved
+			}
+		}
+	}
+
+	for _, n := range toAdd {
+		peerAddService(&n, svcNamespace, svcName)
+		err = m.client.Create(ctx, &n)
+		if err != nil {
+			return false, fmt.Errorf("unable to add BGPPeer %s: %w", n.GetName(), err)
+		}
+		changed = true
+	}
+
+	return changed, nil
+}
+
+// RemovePeersByService remove peers from a particular service.
+// For any peers that have this service in the services Label, remove
+// the service from the label. If there are no services left, remove the
+// peer entirely.
+func (m *CRDConfigurer) RemovePeersByService(ctx context.Context, svcNamespace, svcName string) (bool, error) {
+	var changed bool
+
+	olds, err := m.listBGPPeers(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	for _, o := range olds.Items {
+		removed, err := m.updateOrDeletePeerByService(ctx, o, svcNamespace, svcName)
+		if err != nil {
+			return false, err
+		}
+		if !changed {
+			changed = removed
+		}
+	}
+	return true, nil
+}
+
+// AddAddressPool adds an address pool. If a matching pool already exists, do not change anything.
+// Returns if anything changed.
+func (m *CRDConfigurer) AddAddressPool(ctx context.Context, add *AddressPool, svcNamespace, svcName string) (bool, error) {
+	// ignore empty pool; nothing to add
+	if add == nil {
+		return false, nil
+	}
+
+	olds, err := m.listIPAddressPools(ctx)
+	if err != nil {
+		return false, fmt.Errorf("retrieve a list of IPAddressPools %s %w", m.namespace, err)
+	}
+
+	addIPAddr := convertToIPAddr(*add, m.namespace, svcNamespace, svcName)
+
+	// go through the pools and see if we have one that matches
+	// - if same service name return false
+	//
+	// TODO (ocobleseqx)
+	// - Metallb allows ip address sharing for services, so we need to find a way to share a pool
+	//   EnsureLoadBalancerDeleted filters ips by service tags, so when ip is specified and already exists
+	//   it must be updted to include the new serviceNamespace/service
+	for _, o := range olds.Items {
+		var updateLabels, updateAddresses bool
+		// if same name check services labels
+		if o.GetName() == addIPAddr.GetName() {
+			for k := range o.GetLabels() {
+				if strings.HasPrefix(k, svcLabelKeyPrefix) {
+					osvc := strings.TrimPrefix(k, svcLabelKeyPrefix)
+					if osvc == svcName {
+						// already exists
+						return false, nil
+					}
+				}
+			}
+			// if we got here, none matched exactly, update labels
+			updateLabels = true
+		}
+		for _, addr := range addIPAddr.Spec.Addresses {
+			if slices.Contains(o.Spec.Addresses, addr) {
+				updateAddresses = true
+				break
+			}
+		}
+		if updateLabels || updateAddresses {
+			// update pool
+			patch := client.MergeFrom(o.DeepCopy())
+			if updateLabels {
+				o.Labels[serviceLabelKey(svcName)] = serviceLabelValue(svcNamespace)
+			}
+			if updateAddresses {
+				// update addreses and remove duplicates
+				addresses := append(o.Spec.Addresses, addIPAddr.Spec.Addresses...)
+				slices.Sort(addresses)
+				o.Spec.Addresses = slices.Compact(addresses)
+				o.Spec.Addresses = addresses
+			}
+			err := m.client.Patch(ctx, &o, patch)
+			if err != nil {
+				return false, fmt.Errorf("unable to update IPAddressPool %s: %w", o.GetName(), err)
+			}
+			return true, nil
+		}
+	}
+
+	// if we got here, none matched exactly, so add it
+	err = m.client.Create(ctx, &addIPAddr)
+	if err != nil {
+		return false, fmt.Errorf("unable to add IPAddressPool %s: %w", addIPAddr.GetName(), err)
+	}
+
+	// - if there's no BGPAdvertisement, create the default BGPAdvertisement
+	// - if default BGPAdvertisement exists, update IPAddressPools
+	advs, err := m.listBGPAdvertisements(ctx)
+	if err != nil {
+		return false, err
+	}
+	if len(advs.Items) == 0 {
+		adv := metalapi.BGPAdvertisement{}
+		adv.SetName(defaultBgpAdvertisement)
+		adv.SetNamespace(m.namespace)
+		adv.SetLabels(map[string]string{cpemLabelKey: cpemLabelValue})
+		adv.Spec.IPAddressPools = []string{addIPAddr.Name}
+		err = m.client.Create(ctx, &adv)
+		if err != nil {
+			return false, fmt.Errorf("unable to add default BGPAdvertisement %s: %w", adv.GetName(), err)
+		}
+	} else {
+		for _, adv := range advs.Items {
+			if adv.Name == defaultBgpAdvertisement {
+				patch := client.MergeFrom(adv.DeepCopy())
+				adv.Spec.IPAddressPools = append(adv.Spec.IPAddressPools, addIPAddr.Name)
+				err := m.client.Patch(ctx, &adv, patch)
+				if err != nil {
+					return false, fmt.Errorf("unable to update BGPAdvertisement %s: %w", adv.GetName(), err)
+				}
+			}
+		}
+	}
+	return true, nil
+}
+
+// RemoveAddressPool removes a pool by name. If the matching pool does not exist, do not change anything
+func (m *CRDConfigurer) RemoveAddressPool(ctx context.Context, pool string) error {
+	if pool == "" {
+		return nil
+	}
+
+	olds, err := m.listIPAddressPools(ctx)
+	if err != nil {
+		return err
+	}
+
+	// go through the pools and see if we have a match
+	for _, o := range olds.Items {
+		if o.GetName() == pool {
+			if err := m.client.Delete(ctx, &o); err != nil {
+				return fmt.Errorf("unable to delete pool: %w", err)
+			}
+			klog.V(2).Info("addressPool removed")
+		}
+	}
+
+	// TODO (ocobleseqx) if we manage bgpAdvertisements created by users
+	// we will also want to check/update/remove pools specified in them
+	advs, err := m.listBGPAdvertisements(ctx)
+	if err != nil {
+		return err
+	}
+	for _, adv := range advs.Items {
+		if adv.Name == defaultBgpAdvertisement {
+			for i, p := range adv.Spec.IPAddressPools {
+				if p == pool {
+					if len(adv.Spec.IPAddressPools) > 1 {
+						// there are more pools, just remove pool from the default bgpAdv IPAddressPools list
+						patch := client.MergeFrom(adv.DeepCopy())
+						adv.Spec.IPAddressPools = slices.Delete(adv.Spec.IPAddressPools, i, i+1)
+						err := m.client.Patch(ctx, &adv, patch)
+						if err != nil {
+							return fmt.Errorf("unable to update BGPAdvertisement %s: %w", adv.GetName(), err)
+						}
+					} else {
+						// no pools left, delete default bgpAdv
+						err = m.client.Delete(ctx, &adv)
+						if err != nil {
+							return fmt.Errorf("unable to delete BGPPeer %s: %w", adv.GetName(), err)
+						}
+					}
+				}
+			}
+			break
+		}
+	}
+	return nil
+}
+
+func (m *CRDConfigurer) listBGPPeers(ctx context.Context) (metalapi.BGPPeerList, error) {
+	var err error
+	peerList := metalapi.BGPPeerList{}
+	err = m.client.List(ctx, &peerList, client.MatchingLabels{cpemLabelKey: cpemLabelValue}, client.InNamespace(m.namespace))
+	if err != nil {
+		err = fmt.Errorf("unable to retrieve a list of BGPPeers: %w", err)
+	}
+	return peerList, err
+}
+
+func (m *CRDConfigurer) listBGPAdvertisements(ctx context.Context) (metalapi.BGPAdvertisementList, error) {
+	var err error
+	bgpAdvList := metalapi.BGPAdvertisementList{}
+	err = m.client.List(ctx, &bgpAdvList, client.MatchingLabels{cpemLabelKey: cpemLabelValue}, client.InNamespace(m.namespace))
+	if err != nil {
+		err = fmt.Errorf("unable to retrieve a list of BGPAdvertisements: %w", err)
+	}
+	return bgpAdvList, err
+}
+
+func (m *CRDConfigurer) listIPAddressPools(ctx context.Context) (metalapi.IPAddressPoolList, error) {
+	var err error
+	poolList := metalapi.IPAddressPoolList{}
+	err = m.client.List(ctx, &poolList, client.MatchingLabels{cpemLabelKey: cpemLabelValue}, client.InNamespace(m.namespace))
+	if err != nil {
+		err = fmt.Errorf("unable to retrieve a list of IPAddressPools: %w", err)
+	}
+	return poolList, err
+}
+
+func (m *CRDConfigurer) updateOrDeletePeerByService(ctx context.Context, o metalapi.BGPPeer, svcNamespace, svcName string) (bool, error) {
+	original := o.DeepCopy()
+	// get the services for which this peer works
+	peerChanged, size := peerRemoveService(&o, svcNamespace, svcName)
+
+	// if service left update it, otherwise delete peer
+	if peerChanged {
+		if size > 0 {
+			err := m.client.Patch(ctx, &o, client.MergeFrom(original))
+			if err != nil {
+				return false, fmt.Errorf("unable to update BGPPeer %s: %w", o.GetName(), err)
+			}
+		} else {
+			err := m.client.Delete(ctx, &o)
+			if err != nil {
+				return false, fmt.Errorf("unable to delete BGPPeer %s: %w", o.GetName(), err)
+			}
+		}
+		return true, nil
+	}
+	return false, nil
+}
+
+func (m *CRDConfigurer) Get(ctx context.Context) error { return nil }
+
+func (m *CRDConfigurer) Update(ctx context.Context) error { return nil }
+
+// RemoveAddressPooByAddress remove a pool by an address name. If the matching pool does not exist, do not change anything
+func (m *CRDConfigurer) RemoveAddressPoolByAddress(ctx context.Context, addrName string) error {
+	return nil
+}
+
+// AddService ensures that the provided service is in the list of linked services.
+func peerAddService(p *metalapi.BGPPeer, svcNamespace, svcName string) bool {
+	var (
+		services = []Resource{
+			{Namespace: svcNamespace, Name: svcName},
+		}
+		selectors []metalapi.NodeSelector
+	)
+	for _, ns := range p.Spec.NodeSelectors {
+		var namespace, name string
+		for k, v := range ns.MatchLabels {
+			switch k {
+			case serviceNameKey:
+				name = v
+			case serviceNameSpaceKey:
+				namespace = v
+			}
+		}
+		// if this was not a service namespace/name selector, just add it
+		if name == "" && namespace == "" {
+			selectors = append(selectors, ns)
+		}
+		if name != "" && namespace != "" {
+			// if it already had it, nothing to do, nothing change
+			if svcNamespace == namespace && svcName == name {
+				return false
+			}
+			services = append(services, Resource{Namespace: namespace, Name: name})
+		}
+	}
+	// replace the NodeSelectors with everything except for the services
+	p.Spec.NodeSelectors = selectors
+
+	// now add the services
+	sort.Sort(Resources(services))
+
+	// if we did not find it, add it
+	for _, svc := range services {
+		p.Spec.NodeSelectors = append(p.Spec.NodeSelectors, metalapi.NodeSelector{
+			MatchLabels: map[string]string{
+				serviceNameSpaceKey: svc.Namespace,
+				serviceNameKey:      svc.Name,
+			},
+		})
+	}
+	return true
+}
+
+// RemoveService removes a given service from the peer. Returns whether or not it was
+// changed, and how many services are left for this peer.
+func peerRemoveService(p *metalapi.BGPPeer, svcNamespace, svcName string) (bool, int) {
+	var (
+		found     bool
+		size      int
+		services  = []Resource{}
+		selectors []metalapi.NodeSelector
+	)
+	for _, ns := range p.Spec.NodeSelectors {
+		var name, namespace string
+		for k, v := range ns.MatchLabels {
+			switch k {
+			case serviceNameKey:
+				name = v
+			case serviceNameSpaceKey:
+				namespace = v
+			}
+		}
+		switch {
+		case name == "" && namespace == "":
+			selectors = append(selectors, ns)
+		case name == svcName && namespace == svcNamespace:
+			found = true
+		case name != "" && namespace != "" && (name != svcName || namespace != svcNamespace):
+			services = append(services, Resource{Namespace: namespace, Name: name})
+		}
+	}
+	// first put back all of the previous selectors except for the services
+	p.Spec.NodeSelectors = selectors
+	// then add all of the services
+	sort.Sort(Resources(services))
+	size = len(services)
+	for _, svc := range services {
+		p.Spec.NodeSelectors = append(p.Spec.NodeSelectors, metalapi.NodeSelector{
+			MatchLabels: map[string]string{
+				serviceNameSpaceKey: svc.Namespace,
+				serviceNameKey:      svc.Name,
+			},
+		})
+	}
+	return found, size
+}
+
+func convertToIPAddr(addr AddressPool, namespace, svcNamespace, svcName string) metalapi.IPAddressPool {
+	ip := metalapi.IPAddressPool{
+		Spec: metalapi.IPAddressPoolSpec{
+			Addresses:     addr.Addresses,
+			AutoAssign:    addr.AutoAssign,
+			AvoidBuggyIPs: addr.AvoidBuggyIPs,
+		},
+	}
+	ip.SetLabels(map[string]string{
+		cpemLabelKey:             cpemLabelValue,
+		serviceLabelKey(svcName): serviceLabelValue(svcNamespace),
+	})
+	ip.SetName(addr.Name)
+	ip.SetNamespace(namespace)
+	return ip
+}
+
+func convertToBGPPeer(peer Peer, namespace, svc string) metalapi.BGPPeer {
+	time, _ := time.ParseDuration(peer.HoldTime)
+	bgpPeer := metalapi.BGPPeer{
+		Spec: metalapi.BGPPeerSpec{
+			MyASN:      peer.MyASN,
+			ASN:        peer.ASN,
+			Address:    peer.Addr,
+			SrcAddress: peer.SrcAddr,
+			Port:       peer.Port,
+			HoldTime:   metav1.Duration{Duration: time},
+			// KeepaliveTime: ,
+			// RouterID: peer.RouterID,
+			NodeSelectors: convertToNodeSelectors(peer.NodeSelectors),
+			Password:      peer.Password,
+			// BFDProfile:
+			// EBGPMultiHop:
+		},
+	}
+	bgpPeer.SetLabels(map[string]string{cpemLabelKey: cpemLabelValue})
+	bgpPeer.SetName(peer.Name)
+	bgpPeer.SetNamespace(namespace)
+	return bgpPeer
+}
+
+func convertToNodeSelectors(legacy NodeSelectors) []metalapi.NodeSelector {
+	nodeSelectors := make([]metalapi.NodeSelector, 0)
+	for _, l := range legacy {
+		nodeSelectors = append(nodeSelectors, convertToNodeSelector(l))
+	}
+	return nodeSelectors
+}
+
+func convertToNodeSelector(legacy NodeSelector) metalapi.NodeSelector {
+	return metalapi.NodeSelector{
+		MatchLabels:      legacy.MatchLabels,
+		MatchExpressions: convertToMatchExpressions(legacy.MatchExpressions),
+	}
+}
+
+func convertToMatchExpressions(legacy []SelectorRequirements) []metalapi.MatchExpression {
+	matchExpressions := make([]metalapi.MatchExpression, 0)
+	for _, l := range legacy {
+		new := metalapi.MatchExpression{
+			Key:      l.Key,
+			Operator: l.Operator,
+			Values:   l.Values,
+		}
+		matchExpressions = append(matchExpressions, new)
+	}
+	return matchExpressions
+}
+
+// peerSpecEqual return true if a peer is identical.
+// Will only check for it in the current Peer p, and not the "other" peer in the parameter.
+func peerSpecEqual(p, o metalapi.BGPPeerSpec) bool {
+	// not matched if any field is mismatched except for NodeSelectors
+	if p.MyASN != o.MyASN || p.ASN != o.ASN || p.Address != o.Address || p.Port != o.Port || p.HoldTime != o.HoldTime ||
+		p.Password != o.Password || p.RouterID != o.RouterID {
+		return false
+	}
+	return true
+}
+
+func serviceLabelKey(svcName string) string {
+	return svcLabelKeyPrefix + svcName
+}
+
+func serviceLabelValue(svcNamespace string) string {
+	return svcLabelValuePrefix + svcNamespace
+}
--- go.mod
+++ go.mod
@@ -10,7 +10,9 @@ require (
 	github.com/hashicorp/go-retryablehttp v0.7.2
 	github.com/pallinder/go-randomdata v1.2.0
 	github.com/spf13/pflag v1.0.5
-	gopkg.in/yaml.v2 v2.4.0
+	go.universe.tf/metallb v0.13.12
+	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e
+	sigs.k8s.io/controller-runtime v0.17.0
 )
 
 require (
@@ -26,9 +28,12 @@ require (
 	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
-	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
+	github.com/evanphx/json-patch/v5 v5.8.0 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.5.1 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/jsonpointer v0.20.2 // indirect
@@ -45,21 +50,21 @@ require (
 	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
-	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/imdario/mergo v0.3.13 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/prometheus/client_golang v1.16.0 // indirect
-	github.com/prometheus/client_model v0.4.0 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
-	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/prometheus/client_golang v1.18.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/spf13/cobra v1.7.0 // indirect
 	github.com/stoewer/go-strcase v1.2.0 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.10 // indirect
@@ -74,18 +79,17 @@ require (
 	go.opentelemetry.io/otel/sdk v1.19.0 // indirect
 	go.opentelemetry.io/otel/trace v1.19.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.19.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.16.0 // indirect
-	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/oauth2 v0.15.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/term v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/genproto v0.0.0-20230803162519-f966b187b2e5 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20230726155614-23370e0ffb3e // indirect
@@ -94,7 +98,9 @@ require (
 	google.golang.org/protobuf v1.32.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/apiextensions-apiserver v0.29.0 // indirect
 )
 
 require (
@@ -113,5 +119,5 @@ require (
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.28.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
-	sigs.k8s.io/yaml v1.4.0 // indirect
+	sigs.k8s.io/yaml v1.4.0
 )
