a4447621d356707b13bb6db74a08071de4149590
Martynas Deveikis <martynasdev@gmail.com>
2025-10-29 16:31:21 +0200

    test: move e2e test ssh runner logic to common_test.go

--- test/common_test.go
+++ test/common_test.go
@@ -1,16 +1,20 @@
 package e2e_test
 
 import (
+	"bytes"
 	"context"
+	"crypto/ed25519"
 	"encoding/base64"
 	"errors"
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/cherryservers/cherrygo/v3"
+	"golang.org/x/crypto/ssh"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 
@@ -91,6 +95,58 @@ func fipCount(fips []cherrygo.IPAddress) int {
 		}
 	}
 	return count
+}
+
+type sshCmdRunner struct {
+	signer ssh.Signer
+}
+
+// run a command via SSH at the given address using bash.
+// On a non-zero exit code, the response string contains stderr.
+func (s sshCmdRunner) run(addr, cmd string) (string, error) {
+	const port = "22"
+
+	cfg := ssh.ClientConfig{
+		User:            "root",
+		Auth:            []ssh.AuthMethod{ssh.PublicKeys(s.signer)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", addr+":"+port, &cfg)
+	if err != nil {
+		return "", fmt.Errorf("failed ssh dial: %w", err)
+	}
+	defer client.Close()
+
+	session, err := client.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("failed to establish session: %w", err)
+	}
+	defer session.Close()
+
+	var b bytes.Buffer
+	var eb bytes.Buffer
+	session.Stdout = &b
+	session.Stderr = &eb
+	if err := session.Run("bash -lc " + strconv.Quote(cmd)); err != nil {
+		return eb.String(), fmt.Errorf("failed to run cmd: %w", err)
+	}
+
+	return b.String(), nil
+}
+
+func newSshCmdRunner() (*sshCmdRunner, error) {
+	_, pri, err := ed25519.GenerateKey(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate ed25519 keys: %w", err)
+	}
+
+	sig, err := ssh.NewSignerFromSigner(pri)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate key signer: %w", err)
+	}
+
+	s := sshCmdRunner{sig}
+	return &s, nil
 }
 
 type node struct {
--- test/fixtures_test.go
+++ test/fixtures_test.go
@@ -1,9 +1,7 @@
 package e2e_test
 
 import (
-	"bytes"
 	"context"
-	"crypto/ed25519"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -85,58 +83,6 @@ func k8sClient(kubeconfig string) (*kubernetes.Clientset, error) {
 		return nil, fmt.Errorf("failed to build k8s clientset: %w", err)
 	}
 	return clientset, nil
-}
-
-type sshCmdRunner struct {
-	signer ssh.Signer
-}
-
-// run a command via SSH at the given address using bash.
-// On a non-zero exit code, the response string contains stderr.
-func (s sshCmdRunner) run(addr, cmd string) (string, error) {
-	const port = "22"
-
-	cfg := ssh.ClientConfig{
-		User:            "root",
-		Auth:            []ssh.AuthMethod{ssh.PublicKeys(s.signer)},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
-	client, err := ssh.Dial("tcp", addr+":"+port, &cfg)
-	if err != nil {
-		return "", fmt.Errorf("failed ssh dial: %w", err)
-	}
-	defer client.Close()
-
-	session, err := client.NewSession()
-	if err != nil {
-		return "", fmt.Errorf("failed to establish session: %w", err)
-	}
-	defer session.Close()
-
-	var b bytes.Buffer
-	var eb bytes.Buffer
-	session.Stdout = &b
-	session.Stderr = &eb
-	if err := session.Run("bash -lc " + strconv.Quote(cmd)); err != nil {
-		return eb.String(), fmt.Errorf("failed to run cmd: %w", err)
-	}
-
-	return b.String(), nil
-}
-
-func newSshCmdRunner() (*sshCmdRunner, error) {
-	_, pri, err := ed25519.GenerateKey(nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate ed25519 keys: %w", err)
-	}
-
-	sig, err := ssh.NewSignerFromSigner(pri)
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate key signer: %w", err)
-	}
-
-	s := sshCmdRunner{sig}
-	return &s, nil
 }
 
 func serverPublicIP(srv cherrygo.Server) (string, error) {
