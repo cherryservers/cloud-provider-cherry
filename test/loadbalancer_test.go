0699f2a07419ea7072ad367223561c76cacddce8
Martynas Deveikis <martynasdev@gmail.com>
2025-10-29 16:11:27 +0200

    Revert "test: run load balancer e2e tests in parallel"
    This reverts commit 2be9cae771c1b54cbc55e25a7984da6fb1fed749.

--- test/loadbalancer_test.go
+++ test/loadbalancer_test.go
@@ -735,7 +735,6 @@ func untilFipCount(ctx context.Context, t *testing.T, projectID, count int) erro
 }
 
 func TestMetalLB(t *testing.T) {
-	t.Parallel()
 	const testName = "kubernetes-ccm-test-lb-metal-lb"
 	const namespace = metav1.NamespaceDefault
 	ctx := t.Context()
@@ -817,7 +816,6 @@ func TestMetalLB(t *testing.T) {
 // just to check if nodes get bgp annotations, even though
 // it's not part of the kube-vip load balancer implementation.
 func TestKubeVipAndNodeAnnotations(t *testing.T) {
-	t.Parallel()
 	const testName = "kubernetes-ccm-test-lb-kube-vip"
 	ctx := t.Context()
 
