a0e04a8ad6f7fad3c2a2baad01888f69b37d93b9
Martynas Deveikis <martynasdev@gmail.com>
2025-10-30 15:25:35 +0200

    e2e-test: clarify exp backoff error message

--- e2etest/backoff/backoff.go
+++ e2etest/backoff/backoff.go
@@ -28,7 +28,7 @@ func DefaultExpBackoffConfig() ExpBackoffConfig {
 	}
 }
 
-// expBackoff retries f until it returns true, a non-nil error or
+// ExpBackoff retries f until it returns true, a non-nil error or
 // stop returns true.
 func ExpBackoff(f func() (bool, error), cfg ExpBackoffConfig, stop func() bool) error {
 	dur := cfg.start
@@ -55,16 +55,16 @@ func DefaultExpBackoffConfigWithContext(ctx context.Context) ExpBackoffConfigWit
 	return ExpBackoffConfigWithContext{ExpBackoffConfig: DefaultExpBackoffConfig(), ctx: ctx}
 }
 
-// expBackoffWithContext wraps expBackoff with a context.
+// ExpBackoffWithContext wraps expBackoff with a context.
 func ExpBackoffWithContext(f func() (bool, error), cfg ExpBackoffConfigWithContext) error {
 	err := ExpBackoff(f, cfg.ExpBackoffConfig, func() bool {
 		return cfg.ctx.Err() != nil
 	})
 	switch {
 	case errors.Is(err, backoffStoppedError{}):
-		return fmt.Errorf("exp backoff context cancelled %w", cfg.ctx.Err())
+		return fmt.Errorf("exp backoff context cancelled: %w", cfg.ctx.Err())
 	case err != nil:
-		return fmt.Errorf("exp backoff cancelled: %w", err)
+		return fmt.Errorf("error while doing exp backoff: %w", err)
 	default:
 		return nil
 	}
