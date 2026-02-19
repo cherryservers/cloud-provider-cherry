2faf8c05bff8f143d91027d4b997db033c7922a1
Martynas Deveikis <martynasdev@gmail.com>
2026-02-19 15:38:30 +0200

    feat: set service load balancer ips via annotations
    Since `Service.Spec.LoadBalancerIP` is deprecated, use
    implementor-specific annotations for desired load balancer IPs instead,
    keeping the old method as a fallback.
    
    For `empty` load balancing implementation, allow a custome annotation
    via a configuration parameter.

--- cherry/loadbalancers/kubevip/kubevip.go
+++ cherry/loadbalancers/kubevip/kubevip.go
@@ -3,10 +3,14 @@ package kubevip
 
 import (
 	"context"
+	"errors"
 
 	"github.com/cherryservers/cloud-provider-cherry/cherry/loadbalancers"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 )
+
+const loadBalancerIPsAnnotation = "kube-vip.io/loadbalancerIPs"
 
 type LB struct {
 }
@@ -16,6 +20,42 @@ type LB struct {
 //nolint:revive // ignore unused error
 func NewLB(k8sclient kubernetes.Interface, config string) *LB {
 	return &LB{}
+}
+
+// ServiceIP returns the load balancer IP for the Service.
+// The boolean result reports whether an IP was found.
+//
+// The IP is determined as follows:
+//  1. If the "kube-vip.io/loadbalancerIPs" annotation is set and non-empty, it is used.
+//  2. Otherwise, if svc.Spec.LoadBalancerIP is set, it is used.
+func (l *LB) ServiceIP(svc *v1.Service) (string, bool) {
+	if svc == nil {
+		return "", false
+	}
+
+	if ip := svc.Annotations[loadBalancerIPsAnnotation]; ip != "" {
+		return ip, true
+	}
+
+	if ip := svc.Spec.LoadBalancerIP; ip != "" {
+		return ip, true
+	}
+
+	return "", false
+}
+
+// SetServiceIP sets a service's load balancer IP annotation "kube-vip.io/loadbalancerIPs".
+func (l *LB) SetServiceIP(svc *v1.Service, ip string) error {
+	if svc == nil {
+		return errors.New("failed to set ip, service is nil")
+	}
+
+	if svc.Annotations == nil {
+		svc.Annotations = make(map[string]string)
+	}
+
+	svc.Annotations[loadBalancerIPsAnnotation] = ip
+	return nil
 }
 
 // AddService add a service
